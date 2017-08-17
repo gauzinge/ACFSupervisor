9234e98bbc64da77070b3c8fd246fc17377f45ac
G. Auzinger <georg.auzinger@cern.ch>
2017-08-17 16:36:40 +0200

    some messing with css etc

--- include/DTCSupervisor/DTCSupervisor.h
+++ include/DTCSupervisor/DTCSupervisor.h
@@ -2,18 +2,56 @@
 #define __DTCSupervisor_H__
 
 #include "xdaq/Application.h"
+#include "xdaq/exception/Exception.h"
+#include "xdaq/NamespaceURI.h"
+#include "xdaq/WebApplication.h"
+
+#include "xgi/Utils.h"
 #include "xgi/Method.h"
+//#include "xgi/framework/UIManager.h"
+
+#include "cgicc/CgiDefs.h"
+#include "cgicc/Cgicc.h"
+#include "cgicc/HTTPHTMLHeader.h"
 #include "cgicc/HTMLClasses.h"
+
+#include "xdata/ActionListener.h"
+#include "xdata/String.h"
+
+enum class Tab {EDITOR, MAIN, CALIBRATION, DAQ};
 
 namespace Ph2TkDAQ {
 
-    class DTCSupervisor : public xdaq::Application
+    //DTC Supervisor main class, responsible for GUI and user input
+    class DTCSupervisor : public xdaq::WebApplication, public xdata::ActionListener//, public xgi::framework::UIManager
     {
       public:
         XDAQ_INSTANTIATOR();
 
         DTCSupervisor (xdaq::ApplicationStub* s) throw (xdaq::exception::Exception);
+        ~DTCSupervisor();
+
+        //xdata:event listener
+        void actionPerformed (xdata::Event& e);
+
+        //views
         void Default (xgi::Input* in, xgi::Output* out) throw (xgi::exception::Exception);
+        void MainPage (xgi::Input* in, xgi::Output* out) throw (xgi::exception::Exception);
+        void EditorPage (xgi::Input* in, xgi::Output* out) throw (xgi::exception::Exception);
+        //void CalibrationPage (xgi::Input* in, xgi::Output* out) throw (xgi::exception::Exception);
+        //void DAQPage (xgi::Input* in, xgi::Output* out) throw (xgi::exception::Exception);
+
+        ///HTML header for Hyperdaq interface
+        void createHtmlHeader (xgi::Output* out, Tab pTab, const std::string& strDest = ".");
+        ///Show FSM status and available transition in an HTML table
+        //void showStateMachineStatus (xgi::Output* out) throw (xgi::exception::Exception);
+
+      protected:
+        xdata::String fHWDescriptionFile;
+        //xgi::framework::UIManager fManager;
+
+        Tab fCurrentPageView;
+
     };
 
 }
--- src/common/DTCSupervisor.cc
+++ src/common/DTCSupervisor.cc
@@ -1,20 +1,106 @@
 #include "DTCSupervisor/DTCSupervisor.h"
 
+using namespace cgicc;
 
+//constructor
 XDAQ_INSTANTIATOR_IMPL (Ph2TkDAQ::DTCSupervisor)
 
 Ph2TkDAQ::DTCSupervisor::DTCSupervisor (xdaq::ApplicationStub* s)
-throw (xdaq::exception::Exception) : xdaq::Application (s)
+throw (xdaq::exception::Exception) : xdaq::WebApplication (s), // xgi::framework::UIManager (this),
+    fHWDescriptionFile ("")
 {
+    //bind xgi and xoap commands to methods
     xgi::bind (this, &DTCSupervisor::Default, "Default");
+    //xgi::bind (this, &DTCSupervisor::MainPage, "MainPage");
+    //xgi::bind (this, &DTCSupervisor::EditorPage, "Editor");
+
+    //make configurable variapbles available in the Application Info Space
+    this->getApplicationInfoSpace()->fireItemAvailable ("HWDescriptionFile", &fHWDescriptionFile);
+    //detect when default values have been set
+    this->getApplicationInfoSpace()->addListener (this, "urn:xdaq-event:setDefaultValues");
+
+    //set the current view to MAIN
+    fCurrentPageView = Tab::MAIN;
+}
+
+//Destructor
+Ph2TkDAQ::DTCSupervisor::~DTCSupervisor() {}
+
+//configure action listener
+void Ph2TkDAQ::DTCSupervisor::actionPerformed (xdata::Event& e)
+{
+    if (e.type() == "urn:xdaq-event:setDefaultValues")
+    {
+        std::stringstream ss;
+        ss << "HW Description file: " << std::string (fHWDescriptionFile) << " set!" << std::endl;
+        ss << "All Default Values set!" << std::endl;
+        LOG4CPLUS_INFO (this->getApplicationLogger(), ss.str() );
+        //here is the listener for FSM state transition commands via xoap
+        //have a look at https://gitlab.cern.ch/cms_tk_ph2/BoardSupervisor/blob/master/src/common/BoardSupervisor.cc
+        //at a later point!
+    }
 }
 
 void Ph2TkDAQ::DTCSupervisor::Default (xgi::Input* in, xgi::Output* out)
 throw (xgi::exception::Exception)
 {
+    this->createHtmlHeader (out, fCurrentPageView);
+    //this->showStateMachineStatus (out);
+
     LOG4CPLUS_INFO (this->getApplicationLogger(), "Default method called!");
-    *out << cgicc::HTMLDoctype (cgicc::HTMLDoctype::eStrict) << std::endl;
-    *out << cgicc::html().set ("lang", "en").set ("dir", "ltr") << std::endl;
-    *out << cgicc::title ("DTC Supervisor") << std::endl;
-    *out << cgicc::a ("Visit the XDAQ Web site").set ("href", "http://xdaq.web.cern.ch") << std::endl;
+}
+
+void Ph2TkDAQ::DTCSupervisor::createHtmlHeader (xgi::Output* out, Tab pTab, const std::string& strDest)
+{
+    out->getHTTPResponseHeader().addHeader ("Content-Type", "text/html");
+    *out << html().set ("lang", "en").set ("dir", "ltr") << std::endl;
+    *out << HTMLDoctype (HTMLDoctype::eStrict) << std::endl;
+
+    *out << head() << std::endl;
+    //Style this thing
+    *out << style() << std::endl;
+    *out << "body {font-family: \"Lato\", sans-serif;\nbackground-color: #f1f1f1\n}" << std::endl;
+    *out << "div.title {\noverflow: hidden;\nborder: none solid #ccc;\nbackground-color: inherit;\n}" << std::endl;
+    *out << "div.tab {\noverflow: hidden;\nborder: 1px solid #ccc;\nbackground-color: #f1f1f1;\n}" << std::endl;
+    *out << "a.button {\nbackground-color: inherit;\nfloat: left;\nborder: none;\noutline: none;\ncursor: pointer;\npadding: 10px 16px;\ntransition:0.3s;\nfont-size:17px;\n}" << std::endl;
+    *out << "a.button:hover {\nbackground-color: #ddd;\n}" << std::endl;
+    *out << "a.button.active {\nbackground-color: #ccc;\n}" << std::endl;
+    *out << style() << std::endl;
+
+    *out << title ("DTC Supervisor")  << std::endl;
+    *out << head() << std::endl;
+
+    //*out <<  a ("Visit the XDAQ Web site").set ("href", "http://xdaq.web.cern.ch") << std::endl;
+    //*out << a ("Visit the Ph2 ACF Gitlab Repo").set ("href", "http://gitlab.cern.ch/cms_tk_ph2/Ph2_ACF.git") << std::endl;
+
+    std::string cTabBarString;
+
+    switch (pTab)
+    {
+        case Tab::MAIN:
+            cTabBarString = "<a href='MainPage' class=\"button active\">MainPage</a>  <a href='EditorPage' class=\"button\">EditorPage</a>  <a href='CalibrationPage' class=\"button\">CalibrationPage</a>  <a href='DAQPage' class=\"button\">DAQPage</a>";
+            break;
+
+            //case Tab::EDITOR:
+            //cTabBarString = "<a href='MainPage' class=\"button\">MainPage</a>  <a href='EditorPage' class=\"button active\">EditorPage</a>  <a href='CalibrationPage' class=\"button\">CalibrationPage</a>  <a href='DAQPage' class=\"button\">DAQPage</a>";
+            //break;
+
+            //case Tab::CALIBRATION:
+            //cTabBarString = "<a href='MainPage' class=\"button\">MainPage</a>  <a href='EditorPage' class=\"button\">EditorPage</a>  <a href='CalibrationPage' class=\"button active\">CalibrationPage</a>  <a href='DAQPage' class=\"button\">DAQPage</a>";
+            //break;
+
+            //case Tab::DAQ:
+            //cTabBarString = "<a href='MainPage' class=\"button\">MainPage</a>  <a href='EditorPage' class=\"button\">EditorPage</a>  <a href='CalibrationPage' class=\"button\">CalibrationPage</a>  <a href='DAQPage' class=\"button active\">DAQPage</a>";
+            //break;
+    }
+
+    *out << "<div class=\"title\"> <h3> DTC Supervisor </h3></div>" << std::endl;
+    *out << "<div class=\"tab\">" << std::endl;
+    *out << cTabBarString << std::endl;
+    *out << "</div>" << std::endl;
+    *out << cgicc::div() << std::endl;
+    *out << body() << std::endl;
+    *out << html() << std::endl;
+
+    //xgi::Utils::getPageHeader ( out, "DTCSupervisor", cTabBarString, getApplicationDescriptor()->getContextDescriptor()->getURL(), getApplicationDescriptor()->getURN(), "/xgi/images/Appkication.jpg" );
 }
